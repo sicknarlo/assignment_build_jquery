ee8dc56b1d3c76032520a1f7288b60f2af7d0f1c
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-08-25 13:29:21 -0400

    hasClass created

--- script.js
+++ script.js
@@ -31,5 +31,19 @@ var jQuery = function(str){
     return this.value.length;
   }
 
+  results.hasClass = function(name) {
+    for (var i = 0; i < this.length(); i++) {
+      var classes = this.value[i].className.split(" ");
+      for (var j = 0; j < classes.length; j++) {
+        if (classes[j] === name) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
   return results;
 }
+
+var $ = function(str) {return new jQuery(str)};
